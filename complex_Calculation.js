0db9ba4222ed14941226fb839278e385fd6541ff
Hamide Bay <hamide.bay@baycom.ch>
2020-12-07 23:00:16 +0100

    Commits added

--- complex_Calculation.js
+++ complex_Calculation.js
@@ -1,5 +1,7 @@
+// Array mit Numbers
 let numbers = [20, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
+// Calculate root
 let number;
 let root = 0;
 for (number = 0; number < numbers.length; number++) {
@@ -7,6 +9,7 @@ for (number = 0; number < numbers.length; number++) {
   console.log(numbers[number] + " Karekökü: " + root);
 }
 
+//calculate square
 let index;
 let squares = 0;
 for (index = 0; index < numbers.length; index++) {
@@ -14,6 +17,7 @@ for (index = 0; index < numbers.length; index++) {
   console.log(numbers[index] + " Karesi: " + squares);
 }
 
+// calculate sum
 let i;
 let sum = 0;
 for (i = 0; i < numbers.length; i++) {
@@ -21,6 +25,8 @@ for (i = 0; i < numbers.length; i++) {
 }
 console.log("Toplami: " + sum);
 
+
+//calculate avarege
 let num;
 let avarege = 0;
 let summe = 0;
@@ -30,12 +36,16 @@ for (num = 0; num < numbers.length; num++) {
 }
 console.log("Ortalamasi: " + avarege);
 
+
+//sort Array
 let sortedArray = numbers.sort(function (a, b) {
   return a - b;
 });
 
+//find smallest Element
 let smallestElement = sortedArray[0];
 console.log("En kücük sayi: " + smallestElement);
 
+//find biggest Element
 let biggestElement = sortedArray[numbers.length - 1];
 console.log("En büyük sayi: " + biggestElement);
